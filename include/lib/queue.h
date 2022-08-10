db5f4616a5bccf0bc7d8e58e1c78d3e40364fb11
Jacob Kerr <jck268@cornell.edu>
2022-08-10 01:48:28 -0400

    Added queue specification

---
+++ include/lib/queue.h
@@ -0,0 +1,30 @@
+/** Implementation of a generic FIFO queue */
+#ifndef _QUEUE_H_
+#define _QUEUE_H_
+
+#include "stdlib/stdtypes.h"
+
+/** Data structure for a generic FIFO queue */
+struct queue {
+    // TODO
+};
+
+/** Initializes a new queue */
+void queue_init(struct queue *q);
+
+/** Returns true if queue is empty, false otherwise */
+bool queue_empty(struct queue *q);
+
+/** Returns the size of the queue */
+size_t queue_size(struct queue *q);
+
+/** Enqueues the given element into the back of the queue */
+void queue_enqueue(struct queue *q, void *elm);
+
+/** Removes and returns the first element in the queue */
+void *queue_dequeue(struct queue *q);
+
+/** Returns the first element in the queue without removal */
+void *queue_peek(struct queue *q);
+
+#endif //_QUEUE_H_
