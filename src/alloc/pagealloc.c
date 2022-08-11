26097c66bf0b031aa8a5bee1c4e14e5b778e4dc7
Jacob Kerr <jck268@cornell.edu>
2022-08-11 18:18:57 -0400

    Fixed linker symbols for pagespace

--- src/alloc/pagealloc.c
+++ src/alloc/pagealloc.c
@@ -1,9 +1,9 @@
 #include "alloc/pagealloc.h"
 
-extern char __pages_start; // linker symbol
-extern char __pages_size; // linker symbol
+extern char __pagespace_start; // linker symbol
+extern char __pagespace_size; // linker symbol
 
-/** The pointer to the start of the pages section of memory where all pages are allocated from */
-static const char *pages = &__pages_start;
-/** The length of the pages section of memory */
-static const unsigned long pages_size = (unsigned long) &__pages_size;
+/** The pointer to the start of the page space, the pages section of memory where all pages are allocated from */
+static const char *pagespace = &__pagespace_start;
+/** The length of the page space */
+static const unsigned long ps_size = (unsigned long) &__pagespace_size;
