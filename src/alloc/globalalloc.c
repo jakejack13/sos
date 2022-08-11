eac865fab4126781b8f293677c25089b46555178
warren yun <wyun2006@gmail.com>
2022-08-11 00:55:27 -0400

    add null checking for find space and find element

--- src/alloc/globalalloc.c
+++ src/alloc/globalalloc.c
@@ -103,7 +103,7 @@ static struct Node *find_space(size_t size)
 static int global_free(void *p)
 {
   struct Node *element = find_element(p);
-  // if(element == NULL) return
+  if(element == NULL) return NULL;
   if (!element->head)
     return 0;
   else
@@ -118,6 +118,7 @@ static int global_free(void *p)
 static void *global_malloc(size_t size)
 {
   struct Node *head = find_space(size);
+  if(head == NULL) return NULL;
   head->allocated = size;
   head->head = true;
   switch_state(head, size);
@@ -129,12 +130,13 @@ static void *global_malloc(size_t size)
 static void *global_realloc(void *p, size_t size)
 {
   struct Node *old_head = find_element(p);
-  if (!old_head->head)
+  if (old_head == NULL || !old_head->head)
     return NULL;                      // Check for invalid pointer
   int old_size = old_head->allocated; // Save allocated size
   global_free(p);
 
   struct Node *head = find_space(size);
+  if(head == NULL) return NULL;
   switch_state(head, size);
   for (int i = 0; i < old_size; i++)
   {
