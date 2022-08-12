3ebbf4fa6ed2976b0487ed493773a77d5c35c1f6
Farhan Naqib <nerdswagboss64@gmail.com>
2022-08-11 20:58:17 -0400

    added all the functions

--- src/lib/queue.c
+++ src/lib/queue.c
@@ -8,7 +8,10 @@ void queue_init(struct queue *q){
 };
 
 void queue_free(struct queue *q){
-    
+    while (q->front != NULL) {
+        queue_dequeue(q);
+    }
+    free(q);
 };
 
 bool queue_empty(struct queue *q){
@@ -20,13 +23,35 @@ size_t queue_size(struct queue *q){
 };
 
 void queue_enqueue(struct queue *q, void *elm){
+    struct node *newnode = (struct node*)malloc(sizeof(struct node));
 
+    newnode->data = elm;
+    newnode->next = NULL;
+
+    if (queue_empty(q)){
+        q->front = newnode;
+    } else {
+        q->back->next = newnode;
+    }
+    q->back=newnode;
+    q->size++;
 };
 
 void *queue_dequeue(struct queue *q){
+    struct node *oldnode = q->front;
+    if (q->front == q->back){
+        q->front = NULL;
+        q->back = NULL;
+    } else {
+        q->front = oldnode->next;
+    }
+    q->size--;
+    void *elm = oldnode->data;
+    free(oldnode);
 
+    return (elm);
 };
 
 void *queue_peek(struct queue *q){
-
+    return q->front;
 };
