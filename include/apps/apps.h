8b4a93d8ff9d640a31a1bc8e30bbb16f8a2735c3
Jacob Kerr <jck268@cornell.edu>
2022-08-28 00:29:59 -0400

    Create a process loader (#16)

---
+++ include/apps/apps.h
@@ -0,0 +1,11 @@
+/** Entrypoints into programs of the system */
+
+#ifndef _APPS_H_
+#define _APPS_H_
+
+#define EXIT_SUCCESS 0
+
+/** The entrypoint of the helloworld program */
+int helloworld_main(int argc, char **argv);
+
+#endif
---
+++ src/apps/helloworld.c
@@ -0,0 +1,13 @@
+#ifndef _HELLOWORLD_APP_C_
+#define _HELLOWORLD_APP_C_
+
+#include "apps/apps.h"
+
+#include "stdlib/stdio.h"
+
+int helloworld_main(int argc, char **argv) {
+    prints("Hello, world!\n");
+    return EXIT_SUCCESS;
+}
+
+#endif //_HELLOWORLD_APP_C_
