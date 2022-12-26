8e0d0d803e59d364bbca2605fd44e34fda3c3c8e
Altanis <38045884+Altanis@users.noreply.github.com>
2022-12-25 23:20:40 -0500

    Create an implementation of a generic map

--- include/lib/map.h
+++ include/lib/map.h
@@ -4,24 +4,31 @@
 
 #include "stdlib/stdtypes.h"
 
-/** Data structure for a generic map */
-struct map {
-    //TODO
+/** Structures for a generic map. */
+struct entry {
+    void* key;
+    void* value;
 };
 
-/** Initializes a new hashtable */
-void map_init(struct map *m);
+struct map {
+    struct entry* table;
+    uint64_t used;
+    uint64_t size;
+};
 
-/** Frees the hashtable from memory */
+/** Initializes a new hashmap. Returns `-1` if not allocated. */
+int map_init(struct map *m, uint64_t size);
+
+/** Frees the hashtable from memory. */
 void map_free(struct map *m);
 
-/** Adds the key/value pair to the hashtable */
+/** Adds the key/value pair to the hashtable. */
 void map_put(struct map *m, void *key, void *value);
 
-/** Gets the value associated with the specified key */
+/** Gets the value associated with the specified key. Returns NULL if it doesn't exist. */
 void *map_get(struct map *m, void *key);
 
-/** Returns true if the hashtable contains the key, false otherwise */
-bool map_contains(struct map *m, void *key);
+/** Hash function for strings. */
+uint64_t fnv_1a(char *str);
 
 #endif //_MAP_H_
--- src/lib/map.c
+++ src/lib/map.c
@@ -1,2 +1,25 @@
 #include "lib/map.h"
 
+int map_init(struct map* m, uint64_t size) {
+	m->table = calloc(size, sizeof(struct map));
+	m->size = size;
+
+	/** Ensure map was allocated. */
+	if (m->table == NULL) return -1;
+	else return 0;
+}
+
+void map_free(struct map* m) {
+	free(m->table);
+}
+
+void map_put(struct map* m, void* key, void* value) {
+	uint64_t idx = fnv_1a(key) % m->size;
+	m->table[idx].key = key;
+	m->table[idx].value = value;
+	m->used++;
+}
+
+void* map_get(struct map* m, void* key) {
+	return m->table[fnv_1a(key) % m->size].value;
+}
